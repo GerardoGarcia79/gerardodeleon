1a9823ad8b163ab2333aec9904a80a7109684c82
Gerardo de León García <gerardodlg.dev@gmail.com>
2026-06-03 14:46:32 -0600

    feat: add summary and stack texts

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -1,8 +1,11 @@
+import { useTranslations } from "next-intl";
 import TypewriterText from "@/components/ui/TypewriterText";
 
 const HERO_ROLES = ["Frontend Developer", "Fullstack en progreso"] as const;
 
 const Hero = () => {
+  const t = useTranslations();
+
   return (
     <section
       id="top"
@@ -13,9 +16,18 @@ const Hero = () => {
       <div className="z-10 flex max-w-7xl items-center gap-4 text-center">
         <div className="flex flex-1 flex-col">
           <TypewriterText phrases={HERO_ROLES} className="justify-center" />
-          <h1 className="text-4xl font-bold tracking-tight sm:text-5xl md:text-6xl">
+          <h1 className="text-4xl font-bold tracking-tight sm:text-5xl md:text-6xl text-gradient">
             Gerardo de León García
           </h1>
+          <h3 className="text-lg text-muted-foreground my-4 text-balance tracking-normal">
+            {t("Hero.summary")}
+          </h3>
+          <p className="text-muted-foreground text-sm mb-2">
+            {t("Hero.main stack").toUpperCase()}
+          </p>
+          <p className="text-muted-foreground text-sm mb-2">
+            {t("Hero.complementary stack").toUpperCase()}
+          </p>
         </div>
         <div className="flex flex-1">IDE</div>
       </div>
